19f0211e2b7d096f29f84f0a38066728c6812785
VoN <webdesignervon@gmail.com>
2022-07-20 03:39:45 +0100

    Done with Custom Mouse

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // Hamburger Setup
 const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
 });
+
+// Set Mouse Cursor
+let mouseCursor = document.querySelector('.cursor');
+
+window.addEventListener('mousemove',cursor);
+
+function cursor(e)  {
+    mouseCursor.style.top = e.pageY + "px";
+    mouseCursor.style.left = e.pageX + "px";
+}
